e71f0f685da684df753e86f85bd7a33b49246caa
Yuri Katayama <yurikbcarvalho@gmail.com>
2019-10-26 03:01:18 -0300

    irrelevancias

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Deck", menuName = "TableShip-Wars/Deck", order = 0)]
+public class Deck : ScriptableObject {
+
+    public int[] decklist = new int[10];
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-   [SerializeField]
-   GameGrid grid;
-   public int currentCell;
+    [SerializeField]
+    GameGrid grid;
+    public int currentCell;
     void Start () {
 
     }
-    public void Move(int i){
-        switch(i){
+    public void Move (int i) {
+        switch (i) {
             case 0:
                 currentCell++;
                 break;
@@ -19,20 +19,20 @@ public class PlayerController : MonoBehaviour {
                 currentCell--;
                 break;
             case 2:
-                currentCell+=grid.countX;
+                currentCell += grid.countX;
                 break;
             case 3:
-                currentCell-=grid.countX;
+                currentCell -= grid.countX;
                 break;
         }
-        transform.position=grid.GetCell(currentCell);
+        transform.position = grid.GetCell (currentCell);
     }
     // Update is called once per frame
     void Update () {
-        if(Input.GetKeyDown(KeyCode.RightArrow))Move(0);
-        if(Input.GetKeyDown(KeyCode.LeftArrow))Move(1);
-        if(Input.GetKeyDown(KeyCode.UpArrow))Move(2);
-        if(Input.GetKeyDown(KeyCode.DownArrow))Move(3);
+        if (Input.GetKeyDown (KeyCode.RightArrow)) Move (0);
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) Move (1);
+        if (Input.GetKeyDown (KeyCode.UpArrow)) Move (2);
+        if (Input.GetKeyDown (KeyCode.DownArrow)) Move (3);
     }
 
     public Vector2 LerpMovement (Vector2 posOrigin, Vector2 posDestiny) {
