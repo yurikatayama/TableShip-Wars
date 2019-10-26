f8499124a18506c3901a5d92e2afd887bed0de27
Yuri Katayama <yurikbcarvalho@gmail.com>
2019-10-25 22:11:38 -0300

    naves criptable object

---
+++ Assets/Sprites/Naves.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Naves", menuName = "TableShip-Wars/Naves", order = 0)]
+public class Naves : ScriptableObject {
+
+    public int life, actions, damage;
+
+}
