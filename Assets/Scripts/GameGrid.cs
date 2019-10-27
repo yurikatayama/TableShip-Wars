991859fa1433a5d90c9cfac0273793a4f338a1cf
Arthur Tavares <arth_tavares@hotmail.com>
2019-10-27 00:48:41 -0300

    Comitting Jesus Blessings

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    public int countX=10,countY=20;
+    public int cellCount;
+    [Range(0.1f,5)]
+    public float celSize=1;
+    public bool show;
+    public Sprite tile;
+    private List<SpriteRenderer> tiles=new List<SpriteRenderer>();
+    void Start()
+    {
+        cellCount=countX*countY;
+        for(int i=0;i<cellCount;i++){
+            GameObject go= new GameObject("tler"+i);
+            SpriteRenderer sp=go.AddComponent<SpriteRenderer>();
+            sp.sprite=tile;
+            sp.color=Color.green;
+            tiles.Add(sp);
+            go.transform.position=GetCell(i);
+            go.transform.parent=transform;
+            go.transform.localScale=Vector3.one*celSize;
+        }
+    }
+    public SpriteRenderer GetRenderer(int i){
+        return tiles[i];
+    }
+    public Vector2 GetCell(int i){
+        return new Vector2(i%countX*celSize,i/countX*celSize);
+    }
+    void OnDrawGizmos()
+    {
+        if(!show)return;
+        for(int i=0;i<cellCount;i++){
+            Vector2 vector=GetCell(i);
+            Gizmos.DrawCube(new Vector3(vector.x,vector.y),Vector3.one/4);
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
